536e26677dd1c5011c3e1c5a920325cb13a5f78a
niyueming <niyueming@163.com>
2017-03-03 15:34:42 +0800

    对称加密

--- app/src/main/java/net/nym/fingerprintdemo/MainActivity.java
+++ app/src/main/java/net/nym/fingerprintdemo/MainActivity.java
@@ -4,11 +4,16 @@ import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.AnimationDrawable;
 import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
+import android.os.CancellationSignal;
 import android.os.Handler;
+import android.security.keystore.KeyProperties;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -16,15 +21,32 @@ import android.widget.Toast;
 
 import net.nym.fingerprintlibrary.FingerPrintUtils;
 import net.nym.fingerprintlibrary.dialog.FingerprintDialog;
+import net.nym.fingerprintlibrary.keystore.LocalAndroidKeyStore;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 
 import static android.Manifest.permission.USE_FINGERPRINT;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private String IV = "12345678";
+    private final String data = "敖包3456";
+    private String secretKey;
+    private CancellationSignal cancellationSignal;
+    LocalAndroidKeyStore encryptLocalAndroidKeyStore;
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_encrypt).setOnClickListener(this);
+        findViewById(R.id.btn_decrypt).setOnClickListener(this);
+        cancellationSignal = new CancellationSignal();
+
+        encryptLocalAndroidKeyStore = new LocalAndroidKeyStore();
+        encryptLocalAndroidKeyStore.generateKey(LocalAndroidKeyStore.keyName);
 
     }
 
@@ -45,17 +67,92 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public void onClick(View view){
-        if (!FingerPrintUtils.isKeyguardSecure(this)){
-            Toast.makeText(this,"请在设置界面中开启密码锁屏功能",Toast.LENGTH_LONG).show();
-            return;
-        }
-        if (!FingerPrintUtils.hasEnrolledFingerprints(this)){
-            Toast.makeText(this,"您还没有录入指纹, 请在设置界面录入至少一个指纹",Toast.LENGTH_LONG).show();
-            return;
-        }
-        ActivityCompat.requestPermissions(this,new String[]{USE_FINGERPRINT},0);
+        switch (view.getId()){
+            case R.id.imageView:
+                if (!FingerPrintUtils.isKeyguardSecure(this)){
+                    Toast.makeText(this,"请在设置界面中开启密码锁屏功能",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (!FingerPrintUtils.hasEnrolledFingerprints(this)){
+                    Toast.makeText(this,"您还没有录入指纹, 请在设置界面录入至少一个指纹",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                ActivityCompat.requestPermissions(this,new String[]{USE_FINGERPRINT},0);
+                break;
+            case R.id.btn_encrypt:
+                encrypt();
 
+                break;
+            case R.id.btn_decrypt:
+                decrypt();
+                break;
+        }
+
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void encrypt() {
+        FingerprintManager.CryptoObject cryptoObject = encryptLocalAndroidKeyStore.getCryptoObject(
+                Cipher.ENCRYPT_MODE
+                , null
+        );
+        FingerPrintUtils.authenticate(this, cryptoObject, cancellationSignal, new FingerprintManager.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                FingerprintManager.CryptoObject cryptoObject1 = result.getCryptoObject();
+                if (cryptoObject1 != null){
+                    Cipher cipher = cryptoObject1.getCipher();
+                    try {
+                        byte[] encrypted = cipher.doFinal(data.getBytes());
+                        byte[] IV = cipher.getIV();
+                        String se = Base64.encodeToString(encrypted, Base64.URL_SAFE);
+                        String siv = Base64.encodeToString(IV, Base64.URL_SAFE);
+                        MainActivity.this.IV = siv;
+                        secretKey = se;
+                        Log.e("encrypt","se=" + se + ",siv=" + siv);
+                    } catch (BadPaddingException | IllegalBlockSizeException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void decrypt() {
+        FingerprintManager.CryptoObject cryptoObject = encryptLocalAndroidKeyStore.getCryptoObject(
+                Cipher.DECRYPT_MODE
+                , Base64.decode(IV, Base64.URL_SAFE)
+        );
+        FingerPrintUtils.authenticate(this, cryptoObject, cancellationSignal, new FingerprintManager.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                FingerprintManager.CryptoObject cryptoObject1 = result.getCryptoObject();
+                if (cryptoObject1 != null){
+                    Cipher cipher = cryptoObject1.getCipher();
+                    try {
+                        byte[] decrypted = cipher.doFinal(Base64.decode(secretKey, Base64.URL_SAFE));
+                        Log.e("decrypted","decrypted=" + new String(decrypted) );
+                    } catch (BadPaddingException | IllegalBlockSizeException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cancellationSignal != null){
+            cancellationSignal.cancel();
+            cancellationSignal = null;
+        }
     }
 
     @Override
@@ -81,7 +178,7 @@ public class MainActivity extends AppCompatActivity {
             dialog.setOnFingerPrintCallback(new FingerprintDialog.OnFingerPrintCallback() {
                 @Override
                 public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
-                    Log.e("onAuthenticationSucceeded",result.toString());
+                    Log.e("onAuthentication",result.toString());
                 }
             });
             dialog.show();
